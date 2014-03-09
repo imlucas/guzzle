0166ae85e1bab0a69f39279bd714eaa09d4254a5
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-03-08 21:59:07 -0500

    use gulp to make more gulps

--- bin/guz.js
+++ bin/guz.js
@@ -48,9 +48,61 @@ files['package.json'] = {
 _.extend(files['package.json'], pkgs[pkg.type](pkg)['package.json']);
 console.log('files:', files);
 
-var gulp = require('gulp');
-gulp.dest('./test');
+var gulp = require('gulp'),
+  es = require('event-stream'),
+  File = require('gulp-util').File,
+  exec = require('child_process').exec,
+  path = require('path');
 
+var fakeit = function(name, contents){
+  return es.readable(function (_, fn) {
+    this.emit('data', new File({
+      path: name,
+      contents: new Buffer(contents)
+    }));
+    this.emit('end');
+    fn();
+  });
+};
+
+gulp.task('default', function(){
+  fakeit('.', JSON.stringify(files['package.json'], null, 2))
+    .pipe(gulp.dest('./test/package.json'))
+    .pipe(function(){
+      return es.map(function(file, fn){
+        exec('npm install', {cwd: path.dirname(file.path)}, function(err){
+          fn(err, file);
+        });
+      });
+    }());
+});
+
+
+
+//   function CompileJade(file, enc, cb){
+//     opts.filename = file.path;
+//     file.path = handleExtension(file.path, opts);
+
+//     if(file.isStream()){
+//       this.emit('error', new PluginError('gulp-jade', 'Streaming not supported'));
+//       return cb();
+//     }
+
+//     if(file.isBuffer()){
+//       try {
+//         file.contents = new Buffer(handleCompile(String(file.contents), opts));
+//       } catch(e) {
+//         this.emit('error', e);
+//       }
+//     }
+
+//     this.push(file);
+//     cb();
+//   }
+
+// return through.obj(CompileJade);
+
+gulp.start('default');
 // fs.writeFile('package.json')
 
 
